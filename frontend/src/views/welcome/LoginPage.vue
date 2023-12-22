ee29c9b55393203746959f849187dd58903e0748
huazaiki <1120505436@qq.com>
2023-12-22 10:38:29 +0800

    frontend_V0.0.1

--- frontend/src/views/welcome/LoginPage.vue
+++ frontend/src/views/welcome/LoginPage.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div>
-    <router-view/>
+    登录界面
   </div>
 </template>
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
