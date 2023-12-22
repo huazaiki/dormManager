de5eb7bc3223deb65f2b57bd7e852e93e98d4c2d
huazaiki <1120505436@qq.com>
2023-12-23 05:14:00 +0800

    frontend_V0.0.2 完成登陆界面

--- frontend/src/views/welcome/LoginPage.vue
+++ frontend/src/views/welcome/LoginPage.vue
@@ -1,10 +1,58 @@
 <script setup>
+import {Lock, User} from '@element-plus/icons-vue'
+import {reactive} from "vue";
+
+const form = reactive( {
+  username: '',
+  password: '',
+  remember: false
+})
 
 </script>
 
 <template>
-  <div>
-    登录界面
+  <div style="text-align: center; margin: 0 20px">
+    <div style="margin-top: 150px">
+      <div style="font-size: 25px; font-weight: bold">登录</div>
+      <div style="font-size: 14px; color: grey">在进入系统前，请先输入用户名和密码进行登录</div>
+    </div>
+    <div style="margin-top: 50px">
+      <el-form v-model="form">
+        <el-form-item>
+          <el-input v-model="form.username" maxlength="10" type="text" placeholder="用户名/邮箱">
+            <template #prefix>
+              <el-icon><User/></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="form.password" maxlength="20" type="text" placeholder="密码">
+            <template #prefix>
+              <el-icon><Lock/></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-row>
+          <el-col :span="12" style="text-align: left">
+            <el-form-item>
+              <el-checkbox v-model="form.remember" label="记住我"/>
+            </el-form-item>
+          </el-col>
+          <el-col :span="12" style="text-align: right">
+            <el-link>忘记密码</el-link>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+    <div style="margin-top: 10px">
+      <el-button style="width: 270px" type="success" plain>立即登录</el-button>
+    </div>
+    <el-divider>
+      <span>没有账号？</span>
+    </el-divider>
+    <div>
+      <el-button style="width: 270px" type="warning" plain>立即注册</el-button>
+    </div>
   </div>
 </template>
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const AutoImport = require('unplugin-auto-import/webpack')
-const Components = require('unplugin-vue-components/webpack')
-const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
-// https://vitejs.dev/config/
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig({
   plugins: [
     vue(),
